sirspudd/linux

Add -ck1 version.

Makefile
@@ -10,6 +10,10 @@ NAME = Roaring Lionus
 # Comments in this file are targeted only to the developer, do not
 # expect to learn how to build the kernel reading this file.
 
+CKVERSION = -ck1
+CKNAME = BFS Powered
+EXTRAVERSION := $(EXTRAVERSION)$(CKVERSION)
+
 # o Do not use make's built-in rules and variables
 #   (this increases performance and avoids hard-to-debug behaviour);
 # o Look for make include files relative to root of kernel src

CarDGee/linux-kernel-amdgpu

Makefile
@@ -15,6 +15,10 @@ NAME = Bobtail Squid
 PHONY := _all
 _all:
 
+CKVERSION = -ck1
+CKNAME = MuQSS Powered
+EXTRAVERSION := $(EXTRAVERSION)$(CKVERSION)
+
 # We are using a recursive build, so we need to do a little thinking
 # to get the ordering right.
 #